b04a7fa820ee84ccf0097cca81d6427a042b70bb
ZulayNicolOrtegonPintoADSO <zulyortegon15@gmail.com>
2023-02-14 16:10:52 -0500

    avance d2

---
+++ PEDRO/calcular MIC/calcular MIC/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calcular_MIC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string txtpeso = textBox1.Text;
+            double peso = Double.Parse(txtpeso);
+
+            string txtaltura = textBox2.Text;
+            double altura = Double.Parse(txtaltura);
+
+            double imc = peso / (altura * altura);
+
+            MessageBox.Show("EL indice de masa corporal es: " + imc);
+        }
+    }
+}
